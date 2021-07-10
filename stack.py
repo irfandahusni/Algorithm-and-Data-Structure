5ca3e25810641f8b93430a14196e55df9ab1fc3c
irfandahusni <cfei638@gmail.com>
2021-07-10 23:31:26 +0700

    add stack

---
+++ stack.py
@@ -0,0 +1,14 @@
+from linkedlist import Element, LinkedList
+class Stack(object):
+    def __init__(self, top = None):
+        self.ll = Linkedlist(top)
+    
+    def push(self, new_element):
+        "Ddd a new element onto the top of the stack"
+        pass
+
+    def pop(self):
+        "Remove the first element off the top of the stack and return it"
+        pass
+
+e1 = Element(1)
