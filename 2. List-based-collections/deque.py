74c88b6628f52a8443d78c58e93e67c161244565
irfan <irfandahusnisahid@gmail.com>
2021-09-09 15:06:53 +0700

    add deque

--- 2. List-based-collections/deque.py
+++ 2. List-based-collections/deque.py
@@ -1,7 +1,45 @@
-from collections import deque
-queue = deque(["Eric", "John", "Michael"])
-queue.append("Terry")           # Terry arrives
-queue.append("Graham")          # Graham arrives
-print(queue.popleft())                 # The first to arrive now leaves
-print(queue.popleft())                 # The second to arrive now leaves
-print(queue)                           # Remaining queue in order of arrival
+"""Make a Queue class using a list!
+Hint: You can use any Python list method
+you'd like! Try to write each one in as 
+few lines as possible.
+Make sure you pass the test cases too!"""
+
+class Queue:
+    def __init__(self, head=None):
+        self.storage = [head]
+
+    def enqueue(self, new_element):
+        self.storage.append(new_element)
+
+    def peek(self):
+        return self.storage[0]
+
+    def dequeue(self):
+        dequeue_value = self.storage[0]
+        self.storage = self.storage[1:]
+        return dequeue_value
+    
+# Setup
+q = Queue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test peek
+# Should be 1
+print (q.peek())
+
+# Test dequeue
+# Should be 1
+print (q.dequeue())
+
+# Test enqueue
+q.enqueue(4)
+# Should be 2
+print (q.dequeue())
+# Should be 3
+print (q.dequeue())
+# Should be 4
+print (q.dequeue())
+q.enqueue(5)
+# Should be 5
+print (q.peek())
